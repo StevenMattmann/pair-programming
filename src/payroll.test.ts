baff163797c8930bf0bfb737b128b8944f14c790
Steven Mattmann <steven_mattmann@sluz.ch>
2025-04-30 13:45:03 +0200

    feat: AHV/IV/EO sowie ALV/NBU korrekt abgezogen

--- src/payroll.test.ts
+++ src/payroll.test.ts
@@ -14,4 +14,24 @@ describe("Lohnababrechnung", () => {
         expect(payslip.net).toBe(700);
         expect(payslip.deductions.size).toBe(0);
     })
+
+    test("Test 18 Jähriger mit Lohn 1200 Franken (Alle Abzüge ausser PK )", () =>{
+        const salary : Salary = {
+            born: new Date("2007-01-09"),
+            payday: new Date("2025-01-25"),
+            gross: 1200,
+        };
+
+        const payslip = calculatePayslip(salary);
+
+        expect(payslip.deductions.get("AHV")).toBeCloseTo(1200 * 0.087, 2);
+        expect(payslip.deductions.get("IV")).toBeCloseTo(1200 * 0.014, 2);
+        expect(payslip.deductions.get("EO")).toBeCloseTo(1200 * 0.005, 2);
+        expect(payslip.deductions.get("ALV")).toBeCloseTo(1200 * 0.011, 2);
+        expect(payslip.deductions.get("NBU")).toBeCloseTo(1200 * 0.0073, 2);
+        expect(payslip.deductions.get("PK")).toBeUndefined();
+
+    })
+
+
 })
--- src/payroll.ts
+++ src/payroll.ts
@@ -23,12 +23,31 @@ export type Payslip = {
 };
 
 export function calculatePayslip(salary: Salary): Payslip {
-  // TODO: implement
-  const result: Payslip = {
-    salary: salary,
-    deductions: new Map(),
-    totalDeductions: 0.0,
-    net: salary.gross,
+
+  const deductions: Deductions = new Map();
+  const gross = salary.gross;
+  const annual = gross * 12
+
+
+  const ahvStart = new Date(salary.born.getFullYear() + 17, 0, 1 );
+  const needsAhv = salary.payday >= ahvStart;
+
+  if (needsAhv) {
+    deductions.set("AHV" , + (gross * 0.087).toFixed(2));
+    deductions.set("IV" , + (gross * 0.014).toFixed(2));
+    deductions.set("EO" , + (gross * 0.005).toFixed(2));
+  }
+
+  if (needsAhv && annual >= 2500) {
+    deductions.set("ALV", + (gross * 0.011).toFixed(2));
+    deductions.set("NBU", + (gross * 0.0073).toFixed(2));
+  }
+
+
+  return {
+    salary,
+    deductions,
+    totalDeductions: 0,
+    net: gross,
   };
-  return result;
 }
